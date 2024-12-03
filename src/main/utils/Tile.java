1d1233b00f90b545661920565d77212f44c00c24
Turan <Turan@172.17.32.116>
2024-12-03 12:19:17 +0300

    Tried to create Tile Class, not finished yet

---
+++ src/main/utils/Tile.java
@@ -0,0 +1,54 @@
+package main.utils;
+
+import javafx.util.Pair;
+
+// Tile is used in tile-maps such as building area, playing area etc.
+public class Tile {
+	protected int leftSide;
+	protected int rightSide;
+	protected int topSide;
+	protected int bottomSide;
+	protected char tileType; // E = empty, M = monster, H = hero etc.
+
+	// Normal constructor
+	public Tile(int leftSide, int rightSide, int topSide, int bottomSide, char tileType) {
+		super();
+		this.leftSide = leftSide;
+		this.rightSide = rightSide;
+		this.topSide = topSide;
+		this.bottomSide = bottomSide;
+		this.tileType = tileType;
+	}
+
+	//Constructor without specifying tileType
+	public Tile(int leftSide, int rightSide, int topSide, int bottomSide) {
+		// Using this() function to call above constructor. If we change above, this changes too.
+		this(leftSide, rightSide, topSide, bottomSide, 'E');
+	}
+
+	// toString Method
+	@Override
+	public String toString() {
+		return "Tile [leftSide=" + leftSide + ", rightSide=" + rightSide + ", topSide=" + topSide + ", bottomSide="
+				+ bottomSide + ", tileType=" + tileType + "]";
+	}
+
+	public boolean isInside(Pair<Integer, Integer> point) {
+		int xCoordinate = point.getKey();
+		int yCoordinate = point.getValue();
+		
+		return isInside(xCoordinate, yCoordinate);
+	}
+
+	public boolean isInside(int xCoordinate, int yCoordinate) {
+		// Check if x is in range
+		if ((leftSide <= xCoordinate) || (rightSide >= xCoordinate))
+			return false;
+		
+		// Check if y is in range
+		if ((bottomSide <= yCoordinate) || (topSide >= yCoordinate))
+			return false;
+		
+		return true;
+	}
+}
