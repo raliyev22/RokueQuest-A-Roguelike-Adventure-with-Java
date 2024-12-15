7a8678c8ee8d84b0aaa22dfa3388297a3832da88
Çağrı BİLGİNER <cbilginer21@ku.edu.tr>
2024-12-15 13:56:43 +0300

    enchantment classes

--- src/main/model/ExtraLifeEnchantment.java
+++ src/main/model/ExtraLifeEnchantment.java
@@ -1,4 +1,17 @@
 package main.model;
 
-public class ExtraLifeEnchantment extends Enchantment{
+public class ExtraLifeEnchantment extends Enchantment {
+
+    public ExtraLifeEnchantment(int x, int y) {
+        super(x, y, "Extra Life Enchantment");
+    }
+
+    @Override
+    public void applyEffect(Hero hero) {
+        if (isActive) {
+            hero.addLife(1); // Hero'nun canını 1 artırır
+            System.out.println("Extra Life collected! Hero now has " + hero.getLives() + " lives.");
+            deactivate();
+        }
+    }
 }
