1d04d8e5138da2fe6a2bdddd540e02237830bdb9
TuranBerkeYakarer <tyakarer21@ku.edu.tr>
2024-12-18 23:28:16 +0300

    60 percent finished Images.java, just convertImageToChar remaining

--- src/main/model/Images.java
+++ src/main/model/Images.java
@@ -24,7 +24,7 @@ public class Images extends Application {
 	
 	public static final Image IMAGE_HEART_x2 = new Image("/rokue-like_assets/Heart_x2_32_32.png");
 	public static final Image IMAGE_HEART_x4 = new Image("/rokue-like_assets/Heart_x4_64_64.png");
-
+	
 	public static final Image IMAGE_SMEARED = new Image("/rokue-like_assets/SmearedTile_32_32.png");
 	public static final Image IMAGE_SMEARED_x2 = new Image("/rokue-like_assets/SmearedTile_x2_64_64.png");
 	
@@ -33,14 +33,14 @@ public class Images extends Application {
 	
 	// Images that are used more than once, associated with a char.
 	// In alphabetical order.
-	public static final Image IMAGE_ALLURE_X2 = new Image("/rokue-like_assets/Allure_x2_32_32.png");
+	public final static Image IMAGE_ALLURE_X2 = new Image("/rokue-like_assets/Allure_x2_32_32.png");
 	public static final Image IMAGE_ARCHER_X2 = new Image("/rokue-like_assets/Archer_x2_32_32.png");
 	public static final Image IMAGE_BLOCK_X2 = new Image("/rokue-like_assets/Block_x2_32_40.png");
 	public static final Image IMAGE_BOX_x2 = new Image("/rokue-like_assets/Box_x2_32_42.png");
 	public static final Image IMAGE_BOXONBOX_x2 = new Image("/rokue-like_assets/BoxOnTopOfBox_x2_32_64.png");
 	public static final Image IMAGE_CHESTCLOSED_x2 = new Image("/rokue-like_assets/Chest_Closed_x2_32_32.png");
 	public static final Image IMAGE_CHESTOPEN_x2 = new Image("/rokue-like_assets/Chest_Open_x2_32_32.png");
-	public static final Image IMAGE_CHESTHEART_x2 = new Image("/rokue-like_assets/ChestHeart_x2_32_32.png");
+	public static final Image IMAGE_CHESTHEARTCLOSED_x2 = new Image("/rokue-like_assets/ChestHeart_x2_32_32.png");
 	public static final Image IMAGE_CHESTHEARTOPEN_x2 = new Image("/rokue-like_assets/ChestOpenWithHeart_x2_32_54.png");
 	public static final Image IMAGE_CLOAK_x2 = new Image("/rokue-like_assets/Cloak_x2_32_32.png");
 	public static final Image IMAGE_CUBE_x2 = new Image("/rokue-like_assets/Cube_x2_32_32.png");
@@ -53,7 +53,8 @@ public class Images extends Application {
 	public static final Image IMAGE_TILE_x2 = new Image("/rokue-like_assets/Tile_x2_32_32.png");
 	public static final Image IMAGE_LADDER_x2 = new Image("/rokue-like_assets/TileWithLadder_x2_32_32.png");
 	public static final Image IMAGE_WIZARD_x2 = new Image("/rokue-like_assets/Wizard_x2_32_32.png");
-
+	
+	// All of the assets above, in times two pixels.
 	public static final Image IMAGE_ALLURE_x4 = new Image("/rokue-like_assets/Allure_x4_64_64.png");
 	public static final Image IMAGE_ARCHER_x4 = new Image("/rokue-like_assets/Archer_x4_64_64.png");
 	public static final Image IMAGE_BLOCK_x4 = new Image("/rokue-like_assets/Block_x4_64_80.png");
@@ -61,26 +62,181 @@ public class Images extends Application {
 	public static final Image IMAGE_BOXONBOX_x4 = new Image("/rokue-like_assets/BoxOnTopOfBox_x4_64_128.png");
 	public static final Image IMAGE_CHESTCLOSED_x4 = new Image("/rokue-like_assets/Chest_Closed_x4_64_64.png");
 	public static final Image IMAGE_CHESTOPEN_x4 = new Image("/rokue-like_assets/Chest_Open_x4_64_64.png");
-	public static final Image IMAGE_CHESTHEART_x4 = new Image("/rokue-like_assets/ChestHeart_x4_64_64.png");
+	public static final Image IMAGE_CHESTHEARTCLOSED_x4 = new Image("/rokue-like_assets/ChestHeart_x4_64_64.png");
 	public static final Image IMAGE_CHESTHEARTOPEN_x4 = new Image("/rokue-like_assets/ChestOpenWithHeart_x4_64_108.png");
 	public static final Image IMAGE_CLOAK_x4 = new Image("/rokue-like_assets/Cloak_x4_64_64.png");
 	public static final Image IMAGE_CUBE_x4 = new Image("/rokue-like_assets/Cube_x4_64_64.png");
 	public static final Image IMAGE_FIGHTER_x4 = new Image("/rokue-like_assets/Fighter_x4_64_80.png");
+	public static final Image IMAGE_LADDER_x4 = new Image("/rokue-like_assets/TileWithLadder_x4_64_64.png");
 	public static final Image IMAGE_PILLAR_x4 = new Image("/rokue-like_assets/Pillar_x4_64_128.png");
 	public static final Image IMAGE_PLAYERLEFT_x4 = new Image("/rokue-like_assets/PlayerLeft_x4_64_64.png");
 	public static final Image IMAGE_PLAYERRIGHT_x4 = new Image("/rokue-like_assets/PlayerRight_x4_64_64.png");
 	public static final Image IMAGE_REVEAL_x4 = new Image("/rokue-like_assets/Reveal_x4_64_64.png");
 	public static final Image IMAGE_SKULL_x4 = new Image("/rokue-like_assets/Skull_x4_64_64.png");
 	public static final Image IMAGE_TILE_x4 = new Image("/rokue-like_assets/Tile_x4_64_64.png");
-	public static final Image IMAGE_LADDER_x4 = new Image("/rokue-like_assets/TileWithLadder_x4_64_64.png");
 	public static final Image IMAGE_WIZARD_x4 = new Image("/rokue-like_assets/Wizard_x4_64_64.png");
 	
 	
+	//Takes an Image and finds the char associated with it.
+	// Cannot use switch statements (image is not a primitive type) so idk what to do.
+	// One option is to use the hashCode of all the images.
+	// Other option is to use the toString method.
 	public static char convertImageToChar(Image img) {
+		
 		return 'E';
 	}
 	
+	// Takes a char and finds the image associated with it. Upper case means x2, lower case means x4.
+	public static Image convertCharToImage(char c) {
+		// Unless someone wants to change chars to TileType that I conveniently created just for this purpose, it returns a char.
+		// Although if you want to change it, be careful about x2 and x4, please.
+		// You could also use ints, like 1 = archerx2, -1 = archerx4, 2 = box etc.
+		switch (c) {
+			case 'A' -> {
+				return IMAGE_ARCHER_X2;
+			}
+			case 'B' -> {
+				return IMAGE_BOX_x2;
+			}
+			case 'C' -> {
+				return IMAGE_CHESTCLOSED_x2;
+			}
+			case 'D' -> {
+				return IMAGE_BOXONBOX_x2;
+			}
+			case 'E' -> {
+				return IMAGE_TILE_x2;
+			}
+			case 'F' -> {
+				return IMAGE_FIGHTER_x2;
+			}
+			// Why is G = CUBE? Idk I don't see any better alternatives then randomly assigning chars
+			case 'G' -> { 
+				return IMAGE_CUBE_x2;
+			}
+			case 'H' -> {
+				return IMAGE_CHESTHEARTCLOSED_x2;
+			}
+			case 'J' -> {
+				return IMAGE_BLOCK_X2;
+			}
+			case 'K' -> {
+				return IMAGE_CHESTOPEN_x2;
+			}
+			case 'L' -> {
+				return IMAGE_PLAYERLEFT_x2;
+			}
+			case 'M' -> {
+				return IMAGE_CHESTHEARTOPEN_x2;
+			}
+			case 'N' -> {
+				return IMAGE_ALLURE_X2;
+			}
+			case 'O' -> {
+				return IMAGE_CLOAK_x2;
+			}
+			case 'P' -> {
+				return IMAGE_PILLAR_x2;
+			}
+			case 'Q' -> {
+				return IMAGE_REVEAL_x2;
+			}
+			case 'R' -> {
+				return IMAGE_PLAYERRIGHT_x2;
+			}
+			case 'S' -> {
+				return IMAGE_SKULL_x2;
+			}
+			case 'T' -> {
+				return IMAGE_LADDER_x2;
+			}
+			case 'W' -> {
+				return IMAGE_WIZARD_x2;
+			}
+			// As of writing this, I U V X Y Z are unused. Also Turkish characters, in case we need more.
+
+			// Here are the same chars, but lower case. These return x4 images.
+			case 'a' -> {
+				return IMAGE_ARCHER_x4;
+			}
+			case 'b' -> {
+				return IMAGE_BOX_x4;
+			}
+			case 'c' -> {
+				return IMAGE_CHESTCLOSED_x4;
+			}
+			case 'd' -> {
+				return IMAGE_BOXONBOX_x4;
+			}
+			case 'e' -> {
+				return IMAGE_TILE_x4;
+			}
+			case 'f' -> {
+				return IMAGE_FIGHTER_x4;
+			}
+			case 'g' -> { 
+				return IMAGE_CUBE_x4;
+			}
+			case 'h' -> {
+				return IMAGE_CHESTHEARTCLOSED_x4;
+			}
+			case 'j' -> {
+				return IMAGE_BLOCK_x4;
+			}
+			case 'k' -> {
+				return IMAGE_CHESTOPEN_x4;
+			}
+			case 'l' -> {
+				return IMAGE_PLAYERLEFT_x4;
+			}
+			case 'm' -> {
+				return IMAGE_CHESTHEARTOPEN_x4;
+			}
+			case 'n' -> {
+				return IMAGE_ALLURE_x4;
+			}
+			case 'o' -> {
+				return IMAGE_CLOAK_x4;
+			}
+			case 'p' -> {
+				return IMAGE_PILLAR_x4;
+			}
+			case 'q' -> {
+				return IMAGE_REVEAL_x4;
+			}
+			case 'r' -> {
+				return IMAGE_PLAYERRIGHT_x4;
+			}
+			case 's' -> {
+				return IMAGE_SKULL_x4;
+			}
+			case 't' -> {
+				return IMAGE_LADDER_x4;
+			}
+			case 'w' -> {
+				return IMAGE_WIZARD_x4;
+			}
+
+			default -> throw new AssertionError();
+		}
+	}
+	
 	public static void main(String[] args) {
-		System.out.println(convertImageToChar(IMAGE_UI_CHEST));
+		for (char c = 'A'; c <= 'W'; c++) {
+			if (c == 'I' || c == 'U' || c == 'V') {
+				continue;
+			}
+			System.out.println(c);
+			System.out.println(convertCharToImage(c).toString());
+		}
+
+		for (char c = 'a'; c <= 'w'; c++) {
+			if (c == 'i' || c == 'u' || c == 'v') {
+				continue;
+			}
+			System.out.println(c);
+			System.out.println(convertCharToImage(c).toString());
+		}
+
 	}
 }
