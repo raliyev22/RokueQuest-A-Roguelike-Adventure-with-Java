32e33fb1cfaedf9a81f67946e70c8eef69038235
TuranBerkeYakarer <tyakarer21@ku.edu.tr>
2024-12-18 21:42:10 +0300

    Assets + Started Images Class

--- src/main/model/Images.java
+++ src/main/model/Images.java
@@ -3,13 +3,24 @@ package main.model;
 import javafx.scene.image.Image;
 
 public class Images {
-	static final Image TILE_IMAGE = new Image("/rokue-like_assets/Tile_x2_32_32.png");
-    static final Image CHEST_IMAGE = new Image("/rokue-like_assets/Build_Mode_Chest_Full_View.png");
-    static final Image PILLAR_IMAGE = new Image("/rokue-like_assets/Pillar_x2_32_64.png");
-    static final Image LADDER_IMAGE = new Image("/rokue-like_assets/TileWithLadder_x2_32_32.png");
-    static final Image BOX_IMAGE = new Image("/rokue-like_assets/Box_16_21.png");
-    static final Image BOXONBOX_IMAGE = new Image("/rokue-like_assets/BoxOnTopOfBox_x2_32_64.png");
-    static final Image CUBE_IMAGE = new Image("/rokue-like_assets/Cube_x2_32_32.png");
-    static final Image SKULL_IMAGE = new Image("/rokue-like_assets/Skull_x2_32_32.png");
-    static final Image CHEST_IMAGE = new Image("/rokue-like_assets/Chest_Closed_16_14.png");
+	static final Image IMAGE_TILE_x2 = new Image("/rokue-like_assets/Tile_x2_32_32.png");
+    static final Image IMAGE_UI_CHEST_x2 = new Image("/rokue-like_assets/Build_Mode_Chest_Full_View.png");
+    static final Image IMAGE_PILLAR_x2 = new Image("/rokue-like_assets/Pillar_x2_32_64.png");
+    static final Image IMAGE_LADDER_x2 = new Image("/rokue-like_assets/TileWithLadder_x2_32_32.png");
+    static final Image IMAGE_BOX_x2 = new Image("/rokue-like_assets/Box_16_21.png");
+    static final Image IMAGE_BOXONBOX_x2 = new Image("/rokue-like_assets/BoxOnTopOfBox_x2_32_64.png");
+    static final Image IMAGE_CUBE_x2 = new Image("/rokue-like_assets/Cube_x2_32_32.png");
+    static final Image IMAGE_SKULL_x2 = new Image("/rokue-like_assets/Skull_x2_32_32.png");
+    static final Image IMAGE_CHEST_x2 = new Image("/rokue-like_assets/Chest_Closed_16_14.png");
+
+
+	public char convertImageToChar(Image img) {
+		switch (img) {
+			case TILE_IMAGE_x2:
+				"E";
+				break;
+			default:
+				throw new AssertionError();
+		}
+	}
 }
--- src/main/view/PlayModeOtherView.java
+++ src/main/view/PlayModeOtherView.java
@@ -15,8 +15,12 @@ public class PlayModeOtherView extends Application {
 	protected final int SCENE_HEIGHT = 800;
 	protected final int BLOCK_COUNT = 17;
 	protected final int SIDE_BORDER_COUNT = 12;
+	protected final int HALL_POSITION_X = 50;
+	protected final int HALL_POSITION_Y = 50;
 
 	static final Image tileImage = new Image("/rokue-like_assets/Tile_x4_64_64.png");
+
+	protected PlayModeController playModeController = new PlayModeController();
 
 	public void start(Stage primaryStage) {
 		Pane pane = new Pane();
@@ -28,8 +32,18 @@ public class PlayModeOtherView extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
 
-		TiledHall currentHall = new TiledHall(BLOCK_COUNT, SIDE_BORDER_COUNT, PlayModeController.getPlayModeGrid());
+		TiledHall currentHall = new TiledHall(BLOCK_COUNT, SIDE_BORDER_COUNT, playModeController.getPlayModeGrid());
+		setHallPosition(currentHall, HALL_POSITION_X, HALL_POSITION_Y);
+
+		pane.getChildren().add(currentHall);
 	}
+
+	private void setHallPosition(TiledHall hall, int x, int y) {
+        hall.setLayoutX(x);
+        hall.setLayoutY(y);
+        hall.getGrid().setTopLeftXCordinate(hall.getGrid().topLeftXCoordinate + x);
+        hall.getGrid().setTopLeftYCordinate(hall.getGrid().topLeftYCoordinate + y);
+    }
 	
 	public static void main(String[] args) {
 		launch(args);
