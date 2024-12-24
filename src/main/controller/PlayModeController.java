6c72c24f44647aaf847b02ba044db69cbc9168c1
TuranBerkeYakarer <tyakarer21@ku.edu.tr>
2024-12-24 18:34:50 +0300

    Update PlayModeController.java

--- src/main/controller/PlayModeController.java
+++ src/main/controller/PlayModeController.java
@@ -2,122 +2,180 @@ package main.controller;
 
 import java.security.SecureRandom;
 import java.util.ArrayList;
+
+import javafx.util.Pair;
 import main.model.*;
 import main.utils.*;
 
 public class PlayModeController {
-    protected final int ROW = 10;
-    protected final int COLUMN = 9;
-    protected final int tileWidth = 32;
-    protected final int tileHeight = 32;
-    protected static final int topLeftXCoordinate = 10;
-    protected static final int topLeftYCoordinate = 40;
+	protected final int ROW = 10;
+	protected final int COLUMN = 9;
+	protected final int tileWidth = 32;
+	protected final int tileHeight = 32;
+	protected static final int topLeftXCoordinate = 10;
+	protected static final int topLeftYCoordinate = 40;
 	//public static final SecureRandom rng = new SecureRandom();
-
+	
 	protected final Grid earthHall;
 	protected final Grid airHall;
 	protected final Grid waterHall;
 	protected final Grid fireHall;
-    
-    protected Grid playModeGrid;
-    protected Hero hero;
-    protected ArrayList<Monster> monsters;
+	
+	protected Grid playModeGrid;
+	protected Hero hero;
+	protected ArrayList<Monster> monsters;
 	protected HallType hallType;
 	
-    protected int time;
+	protected int time;
 	
-
-
-    public PlayModeController(Grid earthHall, Grid airHall, Grid waterHall, Grid fireHall) {
+	
+	
+	public PlayModeController(Grid earthHall, Grid airHall, Grid waterHall, Grid fireHall) {
 		this.earthHall = earthHall;
 		this.airHall = airHall;
 		this.waterHall = waterHall;
 		this.fireHall = fireHall;
-
-        playModeGrid = new Grid(ROW, COLUMN, tileWidth, tileHeight, topLeftXCoordinate, topLeftYCoordinate);
+		
+		playModeGrid = new Grid(ROW, COLUMN, tileWidth, tileHeight, topLeftXCoordinate, topLeftYCoordinate);
 		hallType = HallType.EARTH;
 		playModeGrid.copyTileMap(earthHall);
-
+		
 		Tile initialHeroTile = getRandomEmptyTile();
 		int randomXCoordinate = playModeGrid.findXofTile(initialHeroTile);
 		int randomYCoordinate = playModeGrid.findXofTile(initialHeroTile);
-        hero = initializeHero(randomXCoordinate, randomYCoordinate);
-
-        monsters = new ArrayList<>();
-    }
-    
-    public Hero initializeHero(int xCoordinate, int yCoordinate) {
-        Hero hero = new Hero(xCoordinate, yCoordinate);
-        playModeGrid.changeTileWithIndex(hero.getPosX(), hero.getPosY(), hero.getCharType());
-        return hero;
-    }
-    
-    public Monster createMonster(int xCoordinate, int yCoordinate, MonsterType type) {
-        Monster monster = null;
-        switch (type) {
-            case MonsterType.FIGHTER -> {
-                monster = new FighterMonster(xCoordinate,yCoordinate);
-            }
-            case MonsterType.ARCHER -> {
-                monster = new ArcherMonster(xCoordinate,yCoordinate);
-            }
-            case MonsterType.WIZARD -> {
-                monster = new WizardMonster(xCoordinate,yCoordinate);
-            }
-        }
-        monsters.add(monster);
-        playModeGrid.changeTileWithIndex(monster.getX(), monster.getY(), monster.getCharType());
-        return monster;
-    }
-
+		hero = initializeHero(randomXCoordinate, randomYCoordinate);
+		
+		monsters = new ArrayList<>();
+	}
+	
+	public Hero initializeHero(int xCoordinate, int yCoordinate) {
+		Hero hero = new Hero(xCoordinate, yCoordinate);
+		playModeGrid.changeTileWithIndex(hero.getPosX(), hero.getPosY(), hero.getCharType());
+		return hero;
+	}
+	
+	public Monster createMonster(int xCoordinate, int yCoordinate, MonsterType type) {
+		Monster monster = null;
+		switch (type) {
+			case MonsterType.FIGHTER -> {
+				monster = new FighterMonster(xCoordinate,yCoordinate);
+			}
+			case MonsterType.ARCHER -> {
+				monster = new ArcherMonster(xCoordinate,yCoordinate);
+			}
+			case MonsterType.WIZARD -> {
+				monster = new WizardMonster(xCoordinate,yCoordinate);
+			}
+		}
+		monsters.add(monster);
+		playModeGrid.changeTileWithIndex(monster.getX(), monster.getY(), monster.getCharType());
+		return monster;
+	}
+	
 	public Tile getRandomEmptyTile() {
 		SecureRandom rng = new SecureRandom();
 		ArrayList<Tile> emptyTiles = getEmptyTiles();
-
+		
 		int luckyTileInd = rng.nextInt(emptyTiles.size());
-
+		
 		return emptyTiles.get(luckyTileInd);
 	}
-
+	
 	public ArrayList<Tile> getEmptyTiles() {
 		ArrayList<Tile> emptyTiles = new ArrayList<>();
-
+		
 		for (Tile tile: playModeGrid.getTileMap()) {
-			if (isEmptyTile(tile.getTileType())){
+			if (isEmptyTileType(tile.getTileType())){
 				emptyTiles.add(tile);
 			}
 		}
-
+		
 		return emptyTiles;
 	}
-
-	public boolean isEmptyTile(char c) {
+	
+	public boolean isEmptyTileType(char c) {
 		if (c == 'E' || c == 'e') {
 			return true;
 		}
 		return false;
 	}
-    
-    public Grid updateGrid() {
-        this.playModeGrid = new Grid(ROW, COLUMN, tileWidth, tileHeight, topLeftXCoordinate, topLeftYCoordinate);
-        for (Monster monster : monsters) {
-            this.playModeGrid.changeTileWithIndex(monster.getX(), monster.getY(), monster.getCharType());
-        }
-        this.playModeGrid.changeTileWithIndex(hero.getPosX(), hero.getPosY(), hero.getCharType());
-        return this.playModeGrid;
-    }
+	
+	public boolean isWalkableTileType(char c) {
+		if (c == 'E' || c == 'e') {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean checkHeroDirection(Directions dir) {
+		boolean canWalk = false;
+		Tile heroTile = playModeGrid.findTileWithIndex(hero.getPosX(), hero.getPosY());
 
+		switch (dir) {
+			case Directions.NORTH -> {
+				Tile aboveTile = playModeGrid.findNorthTile(heroTile);
+				if (aboveTile != null) {
+					if (isWalkableTileType(aboveTile.getTileType())) {
+						canWalk = true;
+						return true;
+					}
+				}
+			}
+			case Directions.EAST -> {
+				Tile rightTile = playModeGrid.findEastTile(heroTile);
+				if (rightTile != null) {
+					if (isWalkableTileType(rightTile.getTileType())) {
+						canWalk = true;
+						return true;
+					}
+				}
+			}
+			case Directions.SOUTH -> {
+				Tile belowTile = playModeGrid.findSouthTile(heroTile);
+				if (belowTile != null) {
+					if (isWalkableTileType(belowTile.getTileType())) {
+						canWalk = true;
+						return true;
+					}
+				}
+			}
+			case Directions.WEST -> {
+				Tile leftTile = playModeGrid.findWestTile(heroTile);
+				if (leftTile != null) {
+					if (isWalkableTileType(leftTile.getTileType())) {
+						canWalk = true;
+						return true;
+					}
+				}
+			}
+			default -> throw new AssertionError();
+		}
+
+		return canWalk;
+	}
+	
+	public Grid updateGrid() {
+		this.playModeGrid = new Grid(ROW, COLUMN, tileWidth, tileHeight, topLeftXCoordinate, topLeftYCoordinate);
+		for (Monster monster : monsters) {
+			this.playModeGrid.changeTileWithIndex(monster.getX(), monster.getY(), monster.getCharType());
+		}
+		this.playModeGrid.changeTileWithIndex(hero.getPosX(), hero.getPosY(), hero.getCharType());
+		return this.playModeGrid;
+	}
+	
 	public Grid getPlayModeGrid() {
 		return this.playModeGrid;
 	}
-
-    public static void main(String[] args) {
-        PlayModeController playGrid = new PlayModeController(null,null,null,null);
-        Hero hero = playGrid.hero;
-        System.out.println(playGrid.playModeGrid.toString());
-        hero.move(Directions.NORTH);
-        playGrid.updateGrid();
-        System.out.println(playGrid.playModeGrid.toString());
-    }
+	
+	public static void main(String[] args) {
+		PlayModeController playGrid = new PlayModeController(null,null,null,null);
+		Hero hero = playGrid.hero;
+		System.out.println(playGrid.playModeGrid.toString());
+		System.out.println(playGrid.checkHeroDirection(Directions.NORTH));
+		/*
+		playGrid.updateGrid();
+		System.out.println(playGrid.playModeGrid.toString());
+		*/
+	}
 }
 
