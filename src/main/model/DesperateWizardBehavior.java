23750b1356848f3c36d93a0c614506786601e9f6
TuranBerkeYakarer <tyakarer21@ku.edu.tr>
2025-01-06 15:01:59 +0300

    Desperate Wizard Teleportation fix

--- src/main/model/DesperateWizardBehavior.java
+++ src/main/model/DesperateWizardBehavior.java
@@ -15,10 +15,21 @@ public class DesperateWizardBehavior implements WizardBehavior {
 
         Grid grid = controller.getPlayModeGrid();
 
+        Tile oldHeroTile = grid.findTileWithIndex(hero.getPosX(), hero.getPosY());
         grid.changeTileWithIndex(hero.getPosX(), hero.getPosY(), 'E');
+        if (hero.isMoving) {
+            Tile nextTile = grid.findTileUsingDirection(oldHeroTile, hero.movingDirection);
+            nextTile.changeTileType('E');
+            hero.isMoving = false;
+            hero.movingDirection = null;
+        }
+
         hero.setPosX(controller.playModeGrid.findXofTile(randomTile));
         hero.setPosY(controller.playModeGrid.findYofTile(randomTile));
-
+        hero.currentX = randomTile.getLeftSide();
+        hero.currentY = randomTile.getTopSide();
+        hero.targetX = randomTile.getLeftSide();
+        hero.targetY = randomTile.getTopSide();
 
         grid.changeTileWithIndex(hero.getPosX(), hero.getPosY(), hero.getCharType());
         controller.getView().updateHeroPosition(randomTile.getLeftSide(), randomTile.getTopSide());
